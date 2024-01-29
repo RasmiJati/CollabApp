5b73f86c251dc4b977b9edf31f8ae29478649d70
RasmiJati <jatirasmi65@gmail.com>
2024-01-29 18:33:13 +0545

    created enum for priority and taskstatus

---
+++ src/main/java/com/rasmijati/model/TaskStatus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rasmijati.model;
+
+/**
+ *
+ * @author rasmi
+ */
+public enum TaskStatus {
+    OPEN,
+    IN_PROGRESS,
+    COMPLETED
+}
+
