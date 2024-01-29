e81bfe970cf34af5c52bba4bcabdb2e77bfa82e0
RasmiJati <jatirasmi65@gmail.com>
2024-01-29 18:24:06 +0545

    initial commit

---
+++ src/main/java/com/rasmijati/collabapp/CollabApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rasmijati.collabapp;
+
+/**
+ *
+ * @author rasmi
+ */
+public class CollabApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
